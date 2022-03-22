2c13673e55425b5379ce32eb169e8c65089fd732
vagrant <vagrant@homestead>
2022-03-22 06:58:48 +0000

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -12,13 +12,14 @@
 namespace App\Handlers;
 
 use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class ImageUploadHandler
 {
     // 只允许一下后缀名的图片文件上传
     protected $allowed_ext = ["png","jpg","gif","jpeg"];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         // 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
         // 文件夹切割能让查找效率更高
@@ -43,9 +44,35 @@ class ImageUploadHandler
         // 将图片移动到我们的目标存储路径中
         $file->move($upload_path,$filename);
 
+        // 如果限制了图片宽度，就进行裁剪图片
+        if ($max_width && $extension != 'gif') {
+
+            // 此类中封装的函数，用于裁剪图片
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
         // 返回图片路径
         return [
             'path' => "/$folder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        // 先实例化，传参是文件的磁盘路径
+        $image = Image::make($file_path);
+
+        // 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+
+            // 设定宽度是 $max_width, 高度等比例缩放
+            $constraint->aspectRatio();
+
+            // 防止猜图片是尺寸变大
+            $constraint->upsize();
+        });
+
+        // 对图片修改后进行保存
+        $image->save();
     }
 }
